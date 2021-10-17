a5cdd167e408fd09b5954c0b44ffcc1aad117cab
Harshita Singh <singhharshita408@gmail.com>
2021-10-17 12:36:35 +0530

    sum of multiple values

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,14 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+        int returnValue = 0;
+        String[] numbersArray = input.split(",");
+        for (String number : numbersArray) {
+            if (!number.trim().isEmpty()) { 
+                returnValue += Integer.parseInt(number);
+            }
+        }
+        return returnValue;
     }
-
+    
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -3,22 +3,25 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class StringCalculatorShould {
-
+class StringCalculatorShould 
+{
+	StringCalculator stringCalculator = new StringCalculator();
     @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
+    void empty_string_should_return_0(){
         assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
     void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
     @Test
 	void string_with_two_number_should_return_number_as_int(){
-    	StringCalculator stringCalculator = new StringCalculator();
+    	
 		assertEquals(3, stringCalculator.add("1,2"));
 	}
+    @Test
+    void any_amount_Of_Numbers_Return_Values_are_their_Sums() {
+        assertEquals(3+6+15+18+46+33, stringCalculator.add("3,6,15,18,46,33"));
+    }
 }
