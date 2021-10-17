8637702f6e1f6934d103b16e3aa76471f102705a
Harshita Singh <singhharshita408@gmail.com>
2021-10-17 16:10:02 +0530

    add method

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -13,8 +13,17 @@ class StringCalculator {
                 returnValue += Integer.parseInt(number);
             }
         }
-        return returnValue;
-    }
+        if (numbersArray.length > 2) {
+            throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
+        } else {
+            for (String number : numbersArray) {
+                if (!number.isEmpty()) {
+                    Integer.parseInt(number);
+                }
+            }
+            return returnValue;
+        }
+    } 
     
     public int newLines(String numbers) {
         int returnValue = 0;
