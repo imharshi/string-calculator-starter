26f898060df1339f024dfd05fffa9860831663a7
Harshita Singh <singhharshita408@gmail.com>
2021-10-17 11:24:47 +0530

    initial commit

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -1,5 +1,4 @@
 package calculator;
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,4 +16,9 @@ class StringCalculatorShould {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
+    @Test
+	void string_with_two_number_should_return_number_as_int(){
+    	StringCalculator stringCalculator = new StringCalculator();
+		assertEquals(3, stringCalculator.add("1,2"));
+	}
 }
